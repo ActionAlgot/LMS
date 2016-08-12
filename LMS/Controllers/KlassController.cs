2c5972f0207d10fbdfe34e1cad0282008e8fccc8
JoelBerglund <joel.berglund94@gmail.com>
2016-08-12 09:56:28 +0200

    Klass/details

--- LMS/Controllers/KlassController.cs
+++ LMS/Controllers/KlassController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LMS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,5 +15,10 @@ namespace LMS.Controllers
         public ActionResult Index(){
             return View(repo.GetAll());
         }
-    }
+
+		public ActionResult Details(int Id) {
+			return View(repo.GetSpecific(Id));
+		}
+		
+	}
 }
