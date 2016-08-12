e571c2d77fc76c4c43cf4815421ea38b7cb29e5f
JoelBerglund <joel.berglund94@gmail.com>
2016-08-12 09:45:07 +0200

    Klass/Index

---
+++ LMS/Controllers/KlassController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS.Controllers
+{
+	[Authorize]
+    public class KlassController : Controller {
+		private Repositories.KlassRepository repo = new Repositories.KlassRepository();
+        // GET: Klass
+		[Authorize(Roles="Teacher")]
+        public ActionResult Index(){
+            return View(repo.GetAll());
+        }
+    }
+}
---
+++ LMS/Repositories/KlassRepository.cs
@@ -0,0 +1,29 @@
+﻿using LMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS.Repositories {
+	public class KlassRepository {
+		private ApplicationDbContext ctx = new ApplicationDbContext();
+
+		public IEnumerable<Klass> GetAll() {
+			return ctx.Klasses;
+		}
+
+		public Klass GetSpecific(int Id) {
+			return ctx.Klasses.SingleOrDefault(k => k.ID == Id);
+		}
+
+		public void Add(int Id, string UId) {
+			ctx.Klasses.SingleOrDefault(k => k.ID == Id).Members.Add(ctx.Users.SingleOrDefault(u => u.Id == UId));
+			ctx.SaveChanges();
+		}
+
+		public void Remove(int Id, string UId) {
+			ctx.Klasses.SingleOrDefault(k => k.ID == Id).Members.Remove(ctx.Users.SingleOrDefault(u => u.Id == UId));
+			ctx.SaveChanges();
+		}
+	}
+}
