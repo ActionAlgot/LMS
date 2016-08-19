588bd6c5f6e65fc2cb315eb1d373bd97716f1ea1
JoelBerglund <joel.berglund94@gmail.com>
2016-08-19 10:10:51 +0200

    Set up FileRepository, FileController and SharedController and downloading from Shared is working.

---
+++ LMS/Controllers/SharedController.cs
@@ -0,0 +1,13 @@
+﻿using LMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS.Controllers
+{
+    public class SharedFilesController : FileController<SharedFile>
+    {
+    }
+}
---
+++ LMS/Repositories/FileRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LMS.Models;
+using System.Data.Entity;
+
+namespace LMS.Repositories {
+	public class FileRepository<T> where T : File {
+		private ApplicationDbContext ctx = new ApplicationDbContext();
+		private DbSet<T> Files { get { return (DbSet<T>)((typeof(T).Equals(typeof(SharedFile))) ? (IQueryable<File>)ctx.SharedFiles : (IQueryable<File>) ctx.SubmissionFiles); } }
+
+		public File GetSpecific(int ID) {
+			return Files.FirstOrDefault(f => f.ID == ID);
+		}
+
+		public void Add(T file) {
+			Files.Add(file);
+			ctx.SaveChanges();
+		}
+
+	}
+}
