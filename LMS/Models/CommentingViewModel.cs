f295875d941da541b91f17bb4027a657e0047a7d
JoelBerglund <joel.berglund94@gmail.com>
2016-08-24 08:58:44 +0200

    Added comment creation and implemented it in submission/index.

---
+++ LMS/Models/CommentingViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS.Models {
+	public class CommentingViewModel {
+		public Comment Comment { get; set; }
+		public int SubmissionID { get; set; }
+	}
+}
---
+++ LMS/Repositories/CommentRepository.cs
@@ -0,0 +1,16 @@
+﻿using LMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS.Repositories {
+	public class CommentRepository {
+		private ApplicationDbContext ctx = new ApplicationDbContext();
+
+		public void Add(Comment comment) {
+			ctx.Comments.Add(comment);
+			ctx.SaveChanges();
+		}
+	}
+}
