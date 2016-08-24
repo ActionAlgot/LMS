e7e116b16f79f16ad11aa3986351fbbc19506340
horizone <peter@kallgren.se>
2016-08-24 14:44:27 +0200

    Rights annotation now doesn't restrict if you are teacher. Also need second param telling position of klass id in url.

--- LMS/Repositories/AccessRepository.cs
+++ LMS/Repositories/AccessRepository.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Diagnostics;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 /* Work in progress */
 namespace LMS.Repositories
@@ -36,12 +38,23 @@ namespace LMS.Repositories
         }
 
         //Kolla om en student är med i en klass
-        public dynamic UserAttendsKlass(string userId, int KlassId)
+        public bool UserAttendsKlass(string userId, int KlassId)
         {
             Debug.WriteLine("Klass {0} och User {1}", KlassId, userId);
             Klass klass = ctx.Klasses.SingleOrDefault(k => k.ID == KlassId);
             ApplicationUser appUser = ctx.Users.SingleOrDefault(u => u.Id == userId);
             return klass.Members.Any(u => u.Id == appUser.Id);
+        }
+
+        //Kolla om en User är en lärare
+        public bool IsTeacher(string userId)
+        {
+            //ApplicationUser appUser = ctx.Users.SingleOrDefault(u => u.Id == userId);
+            if(UserManager.IsInRole(userId, "Teacher"))
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool IsAdmin(ApplicationUser user)
