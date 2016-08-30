410b2cfaf5b6e20de323482f55b142cbea1611c0
JoelBerglund <joel.berglund94@gmail.com>
2016-08-30 10:33:45 +0200

    Overlapping logic for schedule.

--- LMS/Models/Lecture.cs
+++ LMS/Models/Lecture.cs
@@ -6,18 +6,45 @@ using System.Linq;
 using System.Web;
 
 namespace LMS.Models {
-	public class Lecture {
+
+	public abstract class _lecture {
+		public virtual DateTime Start { get; set; }
+		public virtual DateTime End { get; set; }
+		public abstract string Name { get; }
+		public virtual string Description { get; set; }
+		public virtual string Location { get; set; }
+
+		public IEnumerable<_lecture> GetOverlapping(IEnumerable<_lecture> lectures) {
+			return lectures.Where(that
+				=> this != that
+				&& this.Start < that.End
+				&& that.Start < this.End);
+		}
+		public IEnumerable<_lecture> GetOverlapping(Schedule schedule) {
+			return GetOverlapping(schedule.Lectures);
+		}
+	}
+
+	public class OverlapLecture : _lecture {
+		public override string Name { get { return "Overlap"; } }
+	}
+
+	public class Lecture : _lecture {
 		[Key]
 		public int ID { get; set; }
 
-		public DateTime Start { get; set; }
-		public DateTime End { get; set; }
-		public string Name { get { return Schedule.Klass.Name; } }
-		public string Description { get; set; }
-		public string Location { get; set; }
+		public override DateTime Start { get; set; }
+		public override DateTime End { get; set; }
+		public override string Name { get { return Schedule.Klass.Name; } }
+		public override string Description { get; set; }
+		public override string Location { get; set; }
 
 		public int ScheduleID { get; set; }
 		[ForeignKey("ScheduleID")]
 		public virtual KlassSchedule Schedule { get; set; }
+
+		public IEnumerable<_lecture> GetOverlapping() {
+			return GetOverlapping(Schedule);
+		}
 	}
 }
--- LMS/Models/Schedule.cs
+++ LMS/Models/Schedule.cs
@@ -9,6 +9,64 @@ namespace LMS.Models {
 	public abstract class Schedule {
 		public Schedule() { }
 		public virtual ICollection<Lecture> Lectures { get; set; }
+
+		private class FuckOOPEqualityComparer<T> : IEqualityComparer<T> {
+			private Func<T, T, bool> Comparer;
+			private Func<T, int> Hashcoder;
+
+			public FuckOOPEqualityComparer(Func<T, T, bool> comparer, Func<T, int> hash) {
+				Comparer = comparer;
+				Hashcoder = hash;
+			}
+		
+			public bool Equals(T x, T y){
+				return Comparer(x, y);
+			}
+
+			public int GetHashCode(T obj){
+				return Hashcoder(obj);
+			}
+		}
+
+		//gets timespan where lecrtures overlap n times
+		public IEnumerable<OverlapLecture> GetOverlaps(int minOverlaps){
+			return Lectures
+				.Where(l => l.GetOverlapping(this).Count() >= minOverlaps)
+				.Where(l => l.GetOverlapping(this)
+					.Where(l2 =>	//lectures overlap at least 'minOverlaps' times in a single instant
+						(l2.GetOverlapping(this).Intersect(l.GetOverlapping(this)
+						).Count() >= minOverlaps -1))	//-1 to account for 'l'
+					.Any())
+				.Select(l => {	//'lecture' thats spans over lectures involved in overlap
+					var newL = new OverlapLecture { Start = l.Start, End = l.End };
+					while(newL.GetOverlapping(this).Any()){
+						var seedL = newL;
+						newL = newL.GetOverlapping(this).Aggregate(
+							newL,
+							(fug1, fug2) =>
+								new OverlapLecture {
+									Start = fug1.Start < fug2.Start ? fug1.Start : fug2.Start,
+									End = fug1.End > fug2.End ? fug1.End : fug2.End
+								});
+						if (seedL.Start == newL.Start && seedL.End == newL.End) break;
+					}
+					return newL;
+					})
+				.Distinct(new FuckOOPEqualityComparer<OverlapLecture>(
+					(a, b) => a.Start == b.Start && a.End == b.End,
+					obj => obj.ToString().GetHashCode()))
+				.OrderBy(l => l.Start)	//sort to ensure weirdness does not happen in logiuc below
+				.Aggregate(new List<OverlapLecture>(),	//combine overlapping overlaps
+					(lList, l) =>{
+						var lel = lList.SingleOrDefault(el => el.Start < l.End && l.Start < el.End);
+						if (lel != null) {
+							lel.Start = lel.Start < l.Start ? lel.Start : l.Start;
+							lel.End = lel.End < l.End ? lel.End : l.End;
+						} else lList.Add(l);
+						return lList;
+						}
+				);
+		}
 	}
 
 	public class KlassSchedule : Schedule {
