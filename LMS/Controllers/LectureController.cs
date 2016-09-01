69c547f5f092f9780a0cb18fe33dc2ac069eeaaf
JoelBerglund <joel.berglund94@gmail.com>
2016-09-01 15:44:50 +0200

    Lecture details onclick

---
+++ LMS/Controllers/LectureController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LMS.Repositories;
+
+namespace LMS.Controllers
+{
+    public class LectureController : Controller {
+		private LectureRepository repo = new LectureRepository();
+        public ActionResult Lecture(int ID){
+            return View(repo.get(ID));
+        }
+    }
+}
---
+++ LMS/Repositories/LectureRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LMS.Models;
+
+namespace LMS.Repositories {
+	public class LectureRepository {
+		private ApplicationDbContext ctx = new ApplicationDbContext();
+
+		public Lecture get(int ID) {
+			return ctx.Lectures.FirstOrDefault(l => l.ID == ID);
+		}
+	}
+}
