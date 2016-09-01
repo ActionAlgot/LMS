759a06a9ae47f7ce675901ccf09a95a1760c0410
JoelBerglund <joel.berglund94@gmail.com>
2016-09-01 11:22:03 +0200

    Set up ScheduleWeekViewModel

---
+++ LMS/Models/ScheduleWeekViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS.Models {
+	public class ScheduleWeekViewModel {
+		public Schedule Schedule { get; set; }
+		public int Year { get; set; }
+		public int WeekOfYear { get; set; }
+	}
+}
