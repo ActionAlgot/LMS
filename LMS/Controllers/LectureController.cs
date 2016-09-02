0d0b00eec8219d777d741f75f3a9783459c43ac6
JoelBerglund <joel.berglund94@gmail.com>
2016-09-02 11:39:53 +0200

    'Lecture/****' default return URL

--- LMS/Controllers/LectureController.cs
+++ LMS/Controllers/LectureController.cs
@@ -10,48 +10,54 @@ namespace LMS.Controllers
 {
     public class LectureController : Controller {
 		private LectureRepository repo = new LectureRepository();
+		private string defaultReturnUrl {
+			get {
+				return this.Url.Action("MySchedule", "Schedule", new { }, this.Request.Url.Scheme);
+			}
+		}
+
 
 		//TODO by klass authentication
         public ActionResult Lecture(int ID, string rUrl){
-			ViewBag.rUrl = rUrl ?? "Schedule/MySchedule";
+			ViewBag.rUrl = rUrl ?? defaultReturnUrl;
             return View(repo.get(ID));
         }
 
 		[HttpGet, Authorize(Roles="Teacher")]
 		public ActionResult Edit(int ID, string rUrl) {
-			ViewBag.rUrl = rUrl ?? "Schedule/MySchedule";
+			ViewBag.rUrl = rUrl ?? defaultReturnUrl;
 			return View(repo.get(ID));
 		}
 
 		[HttpPost, ValidateAntiForgeryToken]
 		public ActionResult Edit(Lecture lecture, string rUrl) {
-			ViewBag.rUrl = rUrl ?? "Schedule/MySchedule";
+			ViewBag.rUrl = rUrl ?? defaultReturnUrl;
 			if (ModelState.IsValid && repo.Update(lecture)) return Redirect(ViewBag.rUrl);
 			return Redirect(ViewBag.rUrl);
 		}
 
 		[HttpGet, Authorize(Roles = "Teacher")]
 		public ActionResult Create(int sID, string rUrl) {
-			ViewBag.rUrl = rUrl ?? "Schedule/MySchedule";
+			ViewBag.rUrl = rUrl ?? defaultReturnUrl;
 			return View(new Lecture { ScheduleID = sID });
 		}
 
 		[HttpPost, ValidateAntiForgeryToken]
 		public ActionResult Create(Lecture lecture, string rUrl) {
-			ViewBag.rUrl = rUrl ?? "Schedule/MySchedule";
+			ViewBag.rUrl = rUrl ?? defaultReturnUrl;
 			if (ModelState.IsValid && repo.Add(lecture)) return Redirect(ViewBag.rUrl);
 			return View(lecture);
 		}
 
 		[HttpGet, Authorize(Roles = "Teacher")]
 		public ActionResult Remove(int ID, string rUrl) {
-			ViewBag.rUrl = rUrl ?? "Schedule/MySchedule";
+			ViewBag.rUrl = rUrl ?? defaultReturnUrl;
 			return View(repo.get(ID));
 		}
 
 		[HttpPost, ValidateAntiForgeryToken, ActionName("Remove")]
 		public ActionResult RemoveConfirmed(int ID, string rUrl) {
-			ViewBag.rUrl = rUrl ?? "Schedule/MySchedule";
+			ViewBag.rUrl = rUrl ?? defaultReturnUrl;
 			repo.Delete(ID);
 			return Redirect(ViewBag.rUrl);
 		}
